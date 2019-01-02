eeadfe98b8f82041669d628d976c1f60867490b0
WVargen <sacred_y@163.com>
2019-01-02 15:37:29 +0800

    add library

--- springwebdemo/src/main/java/com/demo/springwebdemo/SpringWebApplication.java
+++ springwebdemo/src/main/java/com/demo/springwebdemo/SpringWebApplication.java
@@ -1,14 +1,15 @@
 package com.demo.springwebdemo;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SpringWebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringWebApplication.class, args);
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(SpringWebApplication.class);
+        builder.bannerMode(Banner.Mode.OFF).run(args);
     }
 
 }
-
